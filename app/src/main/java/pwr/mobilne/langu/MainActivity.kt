6f98acd465a71a38ac748529c6f61999bcf3d167
Masioki <49079199+Masioki@users.noreply.github.com>
2021-04-17 14:47:15 +0200

    Init

---
+++ app/src/main/java/pwr/mobilne/langu/MainActivity.kt
@@ -0,0 +1,17 @@
+package pwr.mobilne.langu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pwr.mobilne.langu.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+    }
+}
