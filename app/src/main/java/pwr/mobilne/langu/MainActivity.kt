1b8b02d64969fe49f2ebae9aa37fc87a75396265
KalinaMichal <michal230915@gmail.com>
2021-05-15 21:36:05 +0200

    Added database - not working in 100 pro

--- app/src/main/java/pwr/mobilne/langu/MainActivity.kt
+++ app/src/main/java/pwr/mobilne/langu/MainActivity.kt
@@ -2,16 +2,19 @@ package pwr.mobilne.langu
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import pwr.mobilne.langu.data.UcWord
+import pwr.mobilne.langu.data.WordEntity
 import pwr.mobilne.langu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var uvm: UcWord
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
+        var germanWord = WordEntity(1,"aa","aaa",1,1)
+        //uvm.addWord(germanWord) - tu jest bug
     }
 }
